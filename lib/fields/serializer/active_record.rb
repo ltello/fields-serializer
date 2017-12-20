4322c3da9b2bc4c6931294267f02fa9a2a23f006
Lorenzo Tello <ltello@zonedigital.com>
2017-12-20 21:37:00 +0100

    always render :id attribute + documentation

--- lib/fields/serializer/active_record.rb
+++ lib/fields/serializer/active_record.rb
@@ -8,8 +8,9 @@ module Fields
 
       class_methods do
   
+        # If key is an association of a given model class
         def association?(key)
-          reflections.keys.include?(key)
+          reflections.keys.include?(key.to_s)
         end
 
         # Convert a list of fields (json_api notation) in a list of associations to be
@@ -23,14 +24,32 @@ module Fields
           fields_to_tree(fields).to_includes
         end
 
+        # Creates new anonymous ActiveModel::Serializer subclass from fields in json api notation:
+        #
+        # Class.new do
+        #   attribute :id
+        #   attribute :non_association_field_1
+        #   attribute :non_association_field_2
+        #   attribute :non_association_field_3
+        #   attribute :non_association_field_4
+        #               ...
+        #
+        #   has_one    :association_field_1, serializer: new_serializer_class_for_association_field_1_nested_fields
+        #     or
+        #   belongs_to :association_field_2, serializer: new_serializer_class_for_association_field_2_nested_fields
+        #     or
+        #   has_many   :association_field_3, serializer: new_serializer_class_for_association_field_3_nested_fields
+        # end              ...
         def fields_serializer(*fields)
           create_serializer_class(fields_to_tree(fields).notation)
         end
 
+        # Creates new anonymous ActiveModel::Serializer subclass from fields in Rails includes notation
         def create_serializer_class(fields)
           klass = self
           Class.new(ActiveModel::Serializer).tap do |new_class|
             new_class.class_eval do
+              attribute :id
               Array.wrap(fields).each do |field|
                 if field.kind_of?(Hash)
                   field.each do |association_name, nested_fields|
@@ -49,20 +68,6 @@ module Fields
 
         def fields_to_tree(*fields)
           array_fields(fields.flatten).inject(FieldsTree.new(self), &:merge!)
-        end
-
-        # Calls:
-        #   has_one :user, serializer: new_serializer_class_for_user_fields
-        #     or
-        #   belongs_to :user, serializer: new_serializer_class_for_user_fields
-        #     or
-        #   has_many :users, serializer: new_serializer_class_for_user_fields
-        #
-        def nested_association(fields)
-          fields.each do |association_name, nested_fields|
-            reflection = reflections[association_name]
-            send(reflection.macro, association_name.to_sym, serializer: reflection.klass.create_serializer_class(nested_fields))
-          end
         end
 
         def array_fields(fields)
