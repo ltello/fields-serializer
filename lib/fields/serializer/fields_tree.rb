0c39a2292e55c85e0bd9cef6f87ee5b4c24ac680
Lorenzo Tello <ltello@zonedigital.com>
2017-12-20 23:36:51 +0100

    document some classes

--- lib/fields/serializer/fields_tree.rb
+++ lib/fields/serializer/fields_tree.rb
@@ -4,6 +4,7 @@ require "active_model_serializers"
 module Fields
   module Serializer
 
+    # A class to store a tree structure of a model klass, its attributes and associations.
     class FieldsTree
       attr_reader :klass, :fields, :associations
 
@@ -17,6 +18,14 @@ module Fields
         self if fields.present? || associations.present?
       end
       
+      # Adds a new field (json api notation) to the tree structure:
+      #
+      #   user_tree.notation
+      #     #=> [:name, :surname, { subjects: [:title, { posts: { comments: :count } }], followers: :nickname }]
+      #
+      #   user_tree.merge!("subjects.posts.date").notation
+      #     #=> [:name, :surname, { subjects: [:title, { posts: [{ comments: :count }, :date] }], followers: :nickname }]
+      #
       def merge!(join_field)
         return self unless join_field.present?
         parent, rest = join_field.to_s.split(".", 2)
@@ -28,6 +37,11 @@ module Fields
         self
       end
       
+      # Return the tree structure in Rails includes notation including both associations and fields
+      #
+      #   user_tree.notation
+      #     #=> [:name, :surname, { subjects: [:title, { posts: :comments }], followers: :nickname }]
+      #
       def notation
         if fields.present?
           if associations.present?
@@ -39,7 +53,12 @@ module Fields
           associations_to_notation.presence
         end
       end
-      
+
+      # Return the tree structure in Rails includes notation including only associations
+      #
+      #   user_tree.notation
+      #     #=> [{ subjects: { posts: :comments }}, :followers]
+      #
       def to_includes
         to_includes = associations.inject([]) do |result, (k, v)|
           v_includes = v.to_includes
